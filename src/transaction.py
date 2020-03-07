f6e35000baf531937829923238ad665e4d06070e
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-03-07 08:41:36 +0000

    Refactor code

--- src/transaction.py
+++ src/transaction.py
@@ -7,15 +7,15 @@ class Transaction():
         self.balance = balance
 
     def format(self):
-        date = self.format_date()
-        credit = self.format_decimals(self.credit)
-        debit = self.format_decimals(self.debit)
-        balance = self.format_decimals(self.balance)
+        date = self.__format_date()
+        credit = self.__format_decimals(self.credit)
+        debit = self.__format_decimals(self.debit)
+        balance = self.__format_decimals(self.balance)
         formatted_transaction = f"{date} || {credit} || {debit} || {balance}"
         return formatted_transaction.replace(" 0.00 ", " ")
 
-    def format_date(self):
+    def __format_date(self):
         return datetime.now().strftime("%m/%d/%Y")
 
-    def format_decimals(self, amount):
+    def __format_decimals(self, amount):
         return "{0:.2f}".format(amount)
--- tests/features/test_print_balance.py
+++ tests/features/test_print_balance.py
@@ -2,11 +2,6 @@ import pytest
 from datetime import datetime
 from src.account import Account
 
-# @pytest.fixture(scope="function", autouse=True)
-# def initialize_account_instance():
-    # prepare something ahead of all tests
-    # print("test")
-
 def test_feature_print_empty_balance(capsys):
     account = Account()
     account.print_statement()
@@ -21,10 +16,8 @@ def test_feature_print_balance_after_two_deposits_and_one_withdraw(capsys):
     account.deposit(1000)
     account.deposit(2000)
     account.withdraw(500)
-    
     account.print_statement()
     out, err = capsys.readouterr()
-
     transaction1 = f"{date} || 1000.00 || || 1000.00"
     transaction2 = f"{date} || 2000.00 || || 3000.00"
     transaction3 = f"{date} || || 500.00 || 2500.00"
